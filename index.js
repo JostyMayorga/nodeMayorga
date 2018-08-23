d655851f62404a32833cdaafd0ff57d8e6640d92
JostyMayorga <josty-12@hotmail.com>
2018-08-23 11:37:39 -0500

    login

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
@@ -10,7 +12,24 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/login', (req, res) => res.render('login', {csrf: 'abc'}));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/process', function(req, res){
+    console.log('datos: '+req.query.form);
+    console.log('nombre: ' + req.body.name);
+    console.log('Email: ' + req.body.email);
+});
+
+var fortune = require('./lib/fortune');
+
+app.get('/about', function (req, res) {
+    res.render('about', { fortune:fortune.getFortune()})
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () => console.log(`express on location ${PORT}`));
 
